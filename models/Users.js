f88c3076ee1e153c2ec20481f849af0b0fd9e775
devlooskie <hello@looskie.com>
2021-01-30 12:02:00 -0500

    Create Users.js

---
+++ models/Users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    betaCode: String,
+    banned: Boolean,
+    confirmed: Boolean,
+    ip: String,
+    codesLeft: Number,
+    icon: String,
+    password: String,
+    memberPlus: Boolean,
+    codes: Array,
+    apiToken: String
+})
+
+module.exports = mongoose.model('Users', UserSchema);
