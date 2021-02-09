3691ac2bea15acb716748c12bb1491ef776994df
devlooskie <hello@looskie.com>
2021-02-09 09:51:56 -0500

    refactor commit messages

--- routes/rawRouter.js
+++ routes/rawRouter.js
@@ -1,13 +1,7 @@
 const routes = require('express').Router();
-const fs = require('fs');
 const Datastore = require('nedb');
 var db = {};
-db.users = new Datastore({ filename: './databases/users' });
 db.link = new Datastore({ filename: './databases/links' });
-db.betaCodes = new Datastore({ filename: './databases/betaCodes' });
-db.plusCodes = new Datastore({ filename: './databases/plusCodes' });
-db.emailTokens = new Datastore({ filename: './databases/emailTokens' });
-db.resetTokens = new Datastore({ filename: './databases/resetTokens' });
 
 routes.get('/', (req, res) => res.redirect('/'))
 
@@ -15,21 +9,22 @@ routes.get('/:documentId', (req, res) => {
     db.link.loadDatabase();
     const document = req.params.documentId;
     try {
-        fs.readFile(`./pastes/${document}.txt`, (err, data) => {
-            if (err) return res.render('error.ejs', { error: 'We couldn\'t find that document or an error occurred!' });
-            res.writeHead(200, { 'Content-type': 'application/json' })
-            res.write(data)
-            res.end();
-            db.link.findOne({ URL: document }, (err, doc) => {
-                if (doc.instantDelete) {
+        db.links.findOne({ URL: document }, (err, document) => {
+            if (err) return res.render('error.ejs', { error: 'An error occurred!' });
+            if (document) {
+                res.writeHead(200, {
+                    'Content-Type': 'application/json'
+                })
+                res.write(document.code)
+                res.end();
+                if (document.instantDelete) {
                     setTimeout(() => {
-                        fs.unlink(`./pastes/${doc.URL}.txt`, err => {
-                            if (err) return err;
-                            db.link.remove({ _id: doc._id })
-                        })
-                    }, 1000);
+                        db.link.remove({ URL: document.URL })
+                    }, 1000)
                 }
-            })
+            } else {
+                res.render('error.ejs', { error: 'We couldn\'t find that document!' });
+            }
         })
     } catch (err) {
         res.render('error.ejs', { error: 'We couldn\'t find that document or an error occurred!' });
