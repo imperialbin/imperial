c6f71e2ebf7f19ef82003786e2fff880fbcdc898
devlooskie <hello@looskie.com>
2021-02-09 09:52:05 -0500

    screenshotting :) pls work

---
+++ utilities/screenshotDocument.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+
+// this ugly lmfao
+module.exports = async (documentId, quality) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ],
+    defaultViewport: null
+  })
+  const page = await browser.newPage();
+  const url = `https://imperialb.in/p/${documentId}`;
+  await page.goto(url)
+    .then(async () => {
+      await page.addStyleTag({ content: '.menu{display: none;} .lines {display: none}' })
+      await page.screenshot({ path: `./public/assets/img/${documentId}.jpg`, quality });
+      await browser.close();
+    })
+}
