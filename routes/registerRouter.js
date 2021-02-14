09f4c74b1d938f9e320ec1aa741f6c97ee42dc53
devlooskie <hello@looskie.com>
2021-02-13 20:00:42 -0500

    refactor commit messages

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -93,7 +93,7 @@ routes.post('/', async (req, res) => {
                             if(err) {
                                 console.log(`Failed to send confirmation email to ${user}!`);
                                 return internalError(email, user);
-                            }                            
+                            }
 
                             db.betaCodes.remove({ betaCode: inviteCode }, (err) => console.log(err))
                             Users.findOneAndUpdate({ codes: { code: inviteCode } }, { $pull: { 'codes': { 'code': inviteCode } } });
