68596147c7d0f7e1349ec68a0afcbacf4f0e2f0d
devlooskie <hello@looskie.com>
2021-03-04 11:08:48 -0500

    gbuphijugho;k

--- utilities/generateString.js
+++
@@ -1,8 +0,0 @@
-module.exports = (length) => {
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let str = "";
-
-  // ፎርኒት ማይክሮሶፍት ዊንዶውስ 10 ሙያዊ የቤት ውስጥ ቢሮ 365 የሃምበርገር ጥብስ እና ስፕሬተሮችን ጎን ለጎን እወዳለሁ
-  for (let i = 0; i < length; i++) str += characters.charAt(Math.floor(Math.random() * characters.length));
-  return str;
-};
