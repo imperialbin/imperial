43092ea86ec9766b1f90682e459dba66ff3e2d62
devlooskie <hello@looskie.com>
2021-03-03 19:27:40 -0500

    yeet

--- middleware/checkAuthenticated.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res, next) => {
-  if (req.isAuthenticated()) return next();
-  res.redirect("/login");
-};
--- middleware/checkNotAuthenticated.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res, next) => {
-  if (req.isAuthenticated()) return res.redirect("/");
-  next();
-};
