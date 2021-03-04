ceaf186f3c2d6b5a270b2a729a7621ad433bb30e
devlooskie <hello@looskie.com>
2021-03-03 19:27:40 -0500

    refactor commit messages

--- utilities/decrypt.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-// Usage decrypt(hashedPassword, documentInfo.code, documentInfo.encryptedIv);
-
-module.exports = (password, cipherText, initVector) => {
-  const hashedPassword = crypto.createHash("sha256").update(password).digest();
-  const decipher = crypto.createDecipheriv("aes256", hashedPassword, Buffer.from(initVector, "hex"));
-  return decipher.update(cipherText, "hex", "utf-8") + decipher.final("utf-8");
-};
--- utilities/encrypt.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-// Usage encrypt(hashedPassword, text, initVector);
-
-module.exports = (password, code, initVector) => {
-  const cipher = crypto.createCipheriv("aes256", password, initVector);
-  return cipher.update(code, "utf-8", "hex") + cipher.final("hex");
-};
