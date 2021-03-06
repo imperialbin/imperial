474570ae38d6075375ba54f9e6dde719550fd9aa
devlooskie <hello@looskie.com>
2021-03-05 21:47:01 -0500

    refactor commit messages

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,18 +1,67 @@
 import { Router, Request, Response } from "express";
+export const routes = Router();
 import { Users } from "../models/Users";
 import Datastore from "nedb";
 import bcrypt from "bcrypt";
 
 // Utilities
-
-// uhhhhhhhhhhhhhhhhhhhhh
 const db = {
   emailTokens: new Datastore({ filename: "./databases/emailTokens" }),
   resetTokens: new Datastore({ filename: "./databases/resetTokens" }),
 };
 
-export const routes = Router();
-
 routes.get("/", (req: Request, res: Response) => {
-  res.send("test auth");
+  res.redirect("/");
+});
+
+routes.get("/:token", (req: Request, res: Response) => {
+  const token = req.params.token;
+  db.emailTokens.loadDatabase();
+  db.emailTokens.findOne({ token }, async (err, tokenInfo) => {
+    if (!tokenInfo)
+      return res.render("error.ejs", {
+        error: "That token has already been used or doesn't exists!",
+      });
+
+    await Users.updateOne(
+      { email: tokenInfo.email },
+      { $set: { confirmed: true } }
+    );
+
+    db.emailTokens.remove({ token });
+    res.redirect("/login");
+  });
+});
+
+routes.post("/resetPassword", (req: Request, res: Response) => {
+  const token = req.body.token;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  db.resetTokens.findOne({ token }, async (err, tokenInfo) => {
+    if (!tokenInfo)
+      return res.render("error.ejs", {
+        error: "That token has already been used!",
+      });
+
+    try {
+      if (password.length < 8) throw "Your password must be 8 characters long!";
+      if (confirmPassword === password) throw "Passwords do not match!";
+
+      const hashedPass = await bcrypt.hash(password, 13);
+      await Users.updateOne(
+        { email: tokenInfo.email },
+        { $set: { password: hashedPass } }
+      );
+
+      res.render("success.ejs", {
+        successMessage: "Successfully reset your password!",
+      });
+      db.resetTokens.remove({ token });
+    } catch (error) {
+      res.render("resetPassword.ejs", {
+        token,
+        error,
+      });
+    }
+  });
 });
