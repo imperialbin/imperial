c99db41b27bf9f5e6ca63b10f43fba7cae7ee89f
devlooskie <hello@looskie.com>
2021-03-05 21:47:05 -0500

    Update compareRouter.ts

--- src/routes/compareRouter.ts
+++ src/routes/compareRouter.ts
@@ -1,15 +1,45 @@
 import { Router, Request, Response } from "express";
+export const routes = Router();
 import Datastore from "nedb";
 
-// Utilities
-
-// uhhhhhhhhhhhhhhhhhhhhh
 const db = {
   link: new Datastore({ filename: "../databases/links" }),
 };
 
-export const routes = Router();
-
 routes.get("/", (req: Request, res: Response) => {
-  res.send("test compare");
+  res.redirect("/");
+});
+
+routes.get("/:documentIdOne/:documentIdTwo", (req: Request, res: Response) => {
+  const documentOne = req.params.documentIdOne;
+  const documentTwo = req.params.documentIdTwo;
+  db.link.loadDatabase();
+  try {
+    db.link.findOne({ URL: documentOne }, (err, documentOneInfo) => {
+      if (err)
+        throw "An internal server error occurred please contact an admin!";
+      if (!documentOneInfo)
+        throw `We couldn't find the document ${documentOne} to compare to ${documentTwo}`;
+      if (documentOneInfo.encrypted)
+        throw `${documentOne} is encrypted! You can not compare encrypted documents!`;
+
+      db.link.findOne({ URL: documentTwo }, (err, documentTwoInfo) => {
+        if (err)
+          throw "An internal server error occurred please contact an admin!";
+        if (!documentTwoInfo)
+          throw `We couldn't find the document ${documentTwo} to compare to ${documentOne}`;
+        if (documentTwoInfo.encrypted)
+          throw `${documentTwo} is encrypted! You can not compare encrypted documents!`;
+
+        res.render("compare.ejs", {
+          documentOne: documentOneInfo.code,
+          documentTwo: documentTwoInfo.code,
+        });
+      });
+    });
+  } catch (error) {
+    res.render("error.ejs", {
+      error,
+    });
+  }
 });
