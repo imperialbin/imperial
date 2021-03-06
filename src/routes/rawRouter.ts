029f81da547850baca343b079aa43aa23b2c6a8b
devlooskie <hello@looskie.com>
2021-03-06 12:23:49 -0500

    raw router

--- src/routes/rawRouter.ts
+++ src/routes/rawRouter.ts
@@ -1,9 +1,63 @@
 import { Router, Request, Response } from "express";
-
-// Middleware
-
 export const routes = Router();
+import Datastore from "nedb";
+
+// Utilities
+import { decrypt } from "../utilities/decrypt";
+import { throwApiError } from "../utilities/throwApiError";
+
+const db = {
+  link: new Datastore({ filename: "./databases/links" }),
+};
 
 routes.get("/", (req: Request, res: Response) => {
-  res.send("test raw");
+  res.redirect("/");
+});
+
+routes.get("/:documentId", (req, res) => {
+  const documentId = req.params.documentId;
+  const password: any = req.query.password || false;
+
+  db.link.loadDatabase();
+  db.link.findOne({ URL: documentId }, (err, document) => {
+    if (err)
+      return throwApiError(
+        res,
+        "An internal server error occurred! Please contact an admin!",
+        500
+      );
+    if (!document)
+      return throwApiError(res, "That document does not exists!", 404);
+    if (document.encrypted && !password)
+      return throwApiError(
+        res,
+        "You need to pass ?password=PASSWORD with your request, since this paste is encrypted!",
+        403
+      );
+
+    let code;
+    if (document.encrypted && password) {
+      try {
+        code = decrypt(password, document.code, document.encryptedIv);
+      } catch (error) {
+        return throwApiError(
+          res,
+          "Incorrect password for encrypted document!",
+          403
+        );
+      }
+    } else {
+      code = document.code;
+    }
+
+    res.setHeader("Content-Type", "text/plain");
+    res.send(code);
+    res.end();
+
+    if (document.instantDelete) {
+      setTimeout(() => {
+        db.link.remove({ URL: documentId });
+      }, 1000);
+    }
+  });
 });
