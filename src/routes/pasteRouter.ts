a82927c2cbed12d1aad514ae508f8023166d94e8
devlooskie <hello@looskie.com>
2021-03-05 21:47:12 -0500

    refactor commit messages

--- src/routes/pasteRouter.ts
+++ src/routes/pasteRouter.ts
@@ -1,9 +1,25 @@
 import { Router, Request, Response } from "express";
-
-// Middleware
-
 export const routes = Router();
+import { Users } from "../models/Users";
+import Datastore from "nedb";
+import fs from "fs";
 
-routes.get("/", (req: Request, res: Response) => {
-  res.send("test paste");
-});
+const db = {
+  link: new Datastore({ filename: "./databases/links" }),
+};
+
+// Utilities
+import { decrypt } from "../utilities/decrypt";
+
+routes.get(
+  [
+    "/:documentId",
+    "/:slugOne/:documentId",
+    "/:slugOne/:slugTwo/:documentId",
+    "/:slugOne/:slugTwo/:slugThree/:documentId",
+  ],
+  (req: Request, res: Response) => {
+    const documentId = req.params.documentId;
+    res.send(documentId);
+  }
+);
