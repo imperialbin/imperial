d305c0b3a79604d35459fbee6ee576e8eb928f4d
devlooskie <hello@looskie.com>
2021-03-16 13:00:07 -0400

    odn thjreg use defgault

--- src/middleware/checkAuthenticated.ts
+++ src/middleware/checkAuthenticated.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 
-export default (req: Request, res: Response, next: NextFunction) => {
+export const checkAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (req.isAuthenticated()) return next();
 
   res.redirect("/login");
--- src/middleware/checkNotAuthenticated.ts
+++ src/middleware/checkNotAuthenticated.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 
-export default (req: Request, res: Response, next: NextFunction) => {
+export const checkNotAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (req.isAuthenticated()) return res.redirect("/");
 
   next();
