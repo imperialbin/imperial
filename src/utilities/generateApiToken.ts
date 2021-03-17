168eda9c176f0e73af06a54a4cdbe669434d45e2
devlooskie <hello@looskie.com>
2021-03-17 10:24:27 -0400

    refactor commit messages

---
+++ src/utilities/generateApiToken.ts
@@ -0,0 +1,10 @@
+export const generateApiToken = (): string => {
+  return "IMPERIAL-xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+    /[xy]/g,
+    (c) => {
+      const r = (Math.random() * 16) | 0,
+        v = c === "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    }
+  );
+};
