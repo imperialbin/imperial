e18415e45688c070c928567e2c838cb177466e25
devlooskie <hello@looskie.com>
2021-03-19 10:54:06 -0400

    optimizations

--- src/utilities/getDocuments.ts
+++ src/utilities/getDocuments.ts
@@ -1,21 +1,16 @@
-import { Documents } from "../models/Documents";
+import { Documents, IDocument } from "../models/Documents";
 
 // Usage getDocuments("", 10).then(console.log);
 
 /* Creator can be undefined or a string, so then when using it - it doesn't yell
 and scream and pout and also because if there is no user I still want to
 check if they have documents */
-export const getDocuments = (creator: string | undefined, amount: number) => {
+export const getDocuments = async (
+  creator: string | undefined,
+  amount: number
+): Promise<Array<IDocument>> => {
   // Returns a promise to get a users documents
-  return new Promise((resolve, reject) => {
-    Documents.find({ creator })
-      .sort({ dateCreated: -1 })
-      .limit(amount)
-      .exec((err, documents) => {
-        if (err)
-          return reject("An error occurred whilst getting users documents");
-
-        resolve(documents);
-      });
-  });
+  return await Documents.find({ creator })
+    .sort({ dateCreated: -1 })
+    .limit(amount);
 };
