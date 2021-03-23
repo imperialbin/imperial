542a5a8b9c074cc89dfac77b80c7d99916aa0302
Alistair Smith <alistairsmith01@gmail.com>
2021-03-23 15:14:00 +0000

    feat: Dockerize all the things

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,5 +1,5 @@
-import { Document, Schema } from "mongoose";
-import { userDatabase } from "../utilities/connectDatabases";
+import { Document, Schema } from 'mongoose';
+import { userDatabase } from '../utilities/connectDatabases';
 
 /* ¡¿ESTÁS LEVANTADO?!  ¡¿ESTÁS BUSCANDO ESA BOLSA ?!  ¡¿ESTÁS CORRIENDO  HACIA
 ESA BOLSA ?! ¡¿ESTÁS VOLANDO  A ESA BOLSA ?! ¡¿ESTÁS CONDUCIENDO A ESA BOLSA ?!
@@ -7,56 +7,57 @@ ESA BOLSA ?! ¡¿ESTÁS VOLANDO  A ESA BOLSA ?! ¡¿ESTÁS CONDUCIENDO A ESA BOL
 LEVANTARTE !!!   ¡¡PORQUE DEBEMOS SER RICOS !! */
 
 export interface UserSettings {
-  clipboard: boolean;
-  longerUrls: boolean;
-  instantDelete: boolean;
-  encrypted: boolean;
-  expiration: number;
-  imageEmbed: boolean;
+	clipboard: boolean;
+	longerUrls: boolean;
+	instantDelete: boolean;
+	encrypted: boolean;
+	expiration: number;
+	imageEmbed: boolean;
 }
 
-export interface IUser extends Document {
-  userId: number;
-  name: string;
-  email: string;
-  betaCode: string;
-  banned: boolean;
-  confirmed: boolean;
-  ip: string;
-  codesLeft: number;
-  icon: string;
-  password: string;
-  memberPlus: boolean;
-  codes: Array<string>;
-  apiToken: string;
-  documentsMade: number;
-  settings: UserSettings;
+export interface IUser {
+	_id: string;
+	userId: number;
+	name: string;
+	email: string;
+	betaCode: string;
+	banned: boolean;
+	confirmed: boolean;
+	ip: string;
+	codesLeft: number;
+	icon: string;
+	password: string;
+	memberPlus: boolean;
+	codes: Array<string>;
+	apiToken: string;
+	documentsMade: number;
+	settings: UserSettings;
 }
 
-export const Users = userDatabase.model<IUser>(
-  "Users",
-  new Schema({
-    userId: Number,
-    name: String,
-    email: String,
-    betaCode: String,
-    banned: Boolean,
-    confirmed: Boolean,
-    ip: String,
-    codesLeft: Number,
-    icon: String,
-    password: String,
-    memberPlus: Boolean,
-    codes: Array,
-    apiToken: String,
-    documentsMade: Number,
-    settings: {
-      clipboard: Boolean,
-      longerUrls: Boolean,
-      instantDelete: Boolean,
-      encrypted: Boolean,
-      expiration: Number,
-      imageEmbed: Boolean,
-    },
-  })
+export const Users = userDatabase.model<IUser & Document>(
+	'Users',
+	new Schema({
+		userId: Number,
+		name: String,
+		email: String,
+		betaCode: String,
+		banned: Boolean,
+		confirmed: Boolean,
+		ip: String,
+		codesLeft: Number,
+		icon: String,
+		password: String,
+		memberPlus: Boolean,
+		codes: Array,
+		apiToken: String,
+		documentsMade: Number,
+		settings: {
+			clipboard: Boolean,
+			longerUrls: Boolean,
+			instantDelete: Boolean,
+			encrypted: Boolean,
+			expiration: Number,
+			imageEmbed: Boolean,
+		},
+	})
 );
