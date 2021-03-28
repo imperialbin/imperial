ceb3565163e8be06f5affc898cd05a1bfae6dc30
devlooskie <hello@looskie.com>
2021-03-28 15:17:23 -0400

    add views

--- src/routes/rawRouter.ts
+++ src/routes/rawRouter.ts
@@ -15,45 +15,50 @@ routes.get("/:documentId", (req, res) => {
   const documentId = req.params.documentId;
   const password: any = req.query.password || false;
 
-  Documents.findOne({ URL: documentId }, (err: string, document: IDocument) => {
-    if (err)
-      return throwApiError(
-        res,
-        "An internal server error occurred! Please contact an admin!",
-        500
-      );
-    if (!document)
-      return throwApiError(res, "That document does not exists!", 404);
-    if (document.encrypted && !password)
-      return throwApiError(
-        res,
-        "You need to pass ?password=PASSWORD with your request, since this paste is encrypted!",
-        403
-      );
-
-    let code;
-    if (document.encrypted && password) {
-      try {
-        code = decrypt(password, document.code, document.encryptedIv!);
-      } catch (error) {
+  Documents.findOne(
+    { URL: documentId },
+    async (err: string, document: IDocument) => {
+      if (err)
         return throwApiError(
           res,
-          "Incorrect password for encrypted document!",
+          "An internal server error occurred! Please contact an admin!",
+          500
+        );
+      if (!document)
+        return throwApiError(res, "That document does not exists!", 404);
+      if (document.encrypted && !password)
+        return throwApiError(
+          res,
+          "You need to pass ?password=PASSWORD with your request, since this paste is encrypted!",
           403
         );
+
+      let code;
+      if (document.encrypted && password) {
+        try {
+          code = decrypt(password, document.code, document.encryptedIv!);
+        } catch (error) {
+          return throwApiError(
+            res,
+            "Incorrect password for encrypted document!",
+            403
+          );
+        }
+      } else {
+        code = document.code;
       }
-    } else {
-      code = document.code;
-    }
 
-    res.setHeader("Content-Type", "text/plain");
-    res.send(code);
-    res.end();
+      res.setHeader("Content-Type", "text/plain");
+      res.send(code);
+      res.end();
 
-    if (document.instantDelete) {
-      setTimeout(async () => {
-        await Documents.deleteOne({ URL: documentId });
-      }, 1000);
+      await Documents.updateOne({ URL: documentId }, { $inc: { views: 1 } });
+
+      if (document.instantDelete) {
+        setTimeout(async () => {
+          await Documents.deleteOne({ URL: documentId });
+        }, 1000);
+      }
     }
-  });
+  );
 });
