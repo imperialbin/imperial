38b9ccbe1875b9c89a1698fafbc20ad51046cd38
devlooskie <hello@looskie.com>
2021-04-02 16:42:03 -0400

    refactor commit messages

--- src/middleware/checkAuthenticated.ts
+++ src/middleware/checkAuthenticated.ts
@@ -1,11 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { Users } from "../models/Users";
 
-export const checkAuthenticated = (
+export const checkAuthenticated = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (req.isAuthenticated()) return next();
+  if (req.isAuthenticated()) {
+    const user = await Users.findOne({ _id: req.user.toString() });
+    if (user && !user.banned) {
+      return next();
+    } else {
+      res.redirect("/logout");
+    }
+  }
 
   res.redirect("/login");
 };
