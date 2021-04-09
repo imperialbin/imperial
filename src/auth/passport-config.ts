1466440baeda582bb0f4c93d49e5af306a73c12c
devlooskie <hello@looskie.com>
2021-04-09 09:52:14 -0400

    refactor commit messages

--- src/auth/passport-config.ts
+++ src/auth/passport-config.ts
@@ -47,8 +47,9 @@ export const initialize = (passport: typeof Passport): void => {
   };
 
   passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
-  passport.serializeUser((user, done) => done(null, user._id));
+  passport.serializeUser((user, done) => done(null, user));
   passport.deserializeUser((id, done) => {
-    Users.findById({ _id: id }).then((user) => done(null, user));
+    Users.findById({ _id: id })
+      .then((user) => done(null, user))
   });
 };
