5d1e8ecf6f998fcd5a6798a3e8d86534c8c3d6c0
devlooskie <hello@looskie.com>
2021-04-09 09:52:14 -0400

    pxseu pls fix for me

--- custom.d.ts
+++ custom.d.ts
@@ -2,9 +2,9 @@ import { Document } from "mongoose";
 import { IUser } from "./src/models/Users";
 
 // this will give you sweet typing :p
-
 declare global {
   namespace Express {
-    type User = IUser & Document;
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface User extends IUser {}
   }
 }
--- src/routes/accountRouter.ts
+++ src/routes/accountRouter.ts
@@ -5,7 +5,6 @@ import { url } from "gravatar";
 import fetch from "node-fetch";
 
 // ENV
-const DEVELOPER_USER = process.env.DEVELOPER_USER;
 const DISCORD_GUILD = process.env.DISCORD_GUILD;
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const DISCORD_ROLE_MEMBER_PLUS = process.env.DISCORD_ROLE_MEMBER_PLUS;
@@ -19,48 +18,30 @@ import { verifyToken } from "../utilities/verifyToken";
 
 export const routes = Router();
 
-routes.get("/", (req: Request, res: Response) => {
-  Users.findOne(
-    { _id: req.user?.toString() },
-    async (err: string, user: IUser) => {
-      if (user) {
-        res.render("account.ejs", {
-          user,
-          error: false,
-          success: false,
-          codeError: false,
-          pfpError: false,
-          documents: await getDocuments(req.user?.toString(), 10),
-        });
-      }
-    }
-  );
+routes.get("/", async (req: Request, res: Response) => {
+  const user = req.user;
+  if (user) {
+    res.render("account.ejs", {
+      user,
+      error: false,
+      success: false,
+      codeError: false,
+      pfpError: false,
+      documents: await getDocuments(user._id.toString(), 10),
+    });
+  }
 });
 
 // Retrieving User data
-routes.post("/me", (req: Request, res: Response) => {
+routes.post("/me", async (req: Request, res: Response) => {
   const password = req.body.password;
-  Users.findOne(
-    { _id: req.user?.toString() },
-    async (err: string, user: IUser) => {
-      if (user) {
-        if (await compare(password, user.password)) {
-          res.setHeader("Content-Type", "text/plain");
-          res.write(user.toString());
-          res.end();
-        } else {
-          res.render("account.ejs", {
-            user,
-            error: false,
-            success: false,
-            codeError: false,
-            pfpError: false,
-            documents: await getDocuments(req.user?.toString(), 10),
-          });
-        }
-      }
-    }
-  );
+  const user = req.user;
+  if (!user || !(await compare(password, user.password)))
+    return res.redirect("/account");
+
+  res.setHeader("Content-Type", "text/plain");
+  res.write(user.toString());
+  res.end();
 });
 
 // Redeeming Plus code
@@ -69,7 +50,7 @@ routes.post("/redeem", async (req: Request, res: Response) => {
   try {
     verifyToken(code); // Wont pass if its invalid
     const user = await Users.findOneAndUpdate(
-      { _id: req.user?.toString() },
+      { _id: req.user?._id.toString() },
       { $set: { memberPlus: true } }
     );
     await fetch(
@@ -88,35 +69,69 @@ routes.post("/redeem", async (req: Request, res: Response) => {
 });
 
 routes.post("/unlinkGithub", async (req: Request, res: Response) => {
-  const _id = req.user?.toString();
+  const _id = req.user?._id.toString();
   await Users.updateOne({ _id }, { $set: { githubAccess: null } });
   res.redirect("/account");
 });
 
 // Resetting password stuff
-routes.post("/resetPasswordForm", (req: Request, res: Response) => {
-  const _id = req.user?.toString();
+routes.post("/resetPasswordForm", async (req: Request, res: Response) => {
+  const user = req.user;
   const oldPassword = req.body.oldPassword;
   const newPassword = req.body.newPassword;
   const confirmPassword = req.body.confirmPassword;
 
-  Users.findOne({ _id }, async (err: string, user: IUser) => {
-    try {
-      if (!(await compare(oldPassword, user.password)))
-        throw "Incorrect old password!";
-      if (newPassword.length < 8)
-        throw "Please make your new password more than 8 characters long!";
-      if (newPassword !== confirmPassword) throw "New passwords do not match!";
+  try {
+    if (!user) throw "No user!";
+    if (!(await compare(oldPassword, user.password)))
+      throw "Incorrect old password!";
+    if (newPassword.length < 8)
+      throw "Please make your new password more than 8 characters long!";
+    if (newPassword !== confirmPassword) throw "New passwords do not match!";
 
-      const hashedPass = await hash(newPassword, 13);
-      await Users.updateOne({ _id }, { $set: { password: hashedPass } });
+    const hashedPass = await hash(newPassword, 13);
+    await Users.updateOne(
+      { _id: user._id },
+      { $set: { password: hashedPass } }
+    );
+    res.render("account.ejs", {
+      user,
+      error: false,
+      success: "Successfully reset your password!",
+      codeError: false,
+      pfpError: false,
+      documents: await getDocuments(req.user?.toString(), 10),
+    });
+  } catch (error) {
+    res.render("account.ejs", {
+      user,
+      error,
+      success: false,
+      codeError: false,
+      pfpError: false,
+      documents: await getDocuments(req.user?.toString(), 10),
+    });
+  }
+});
+
+routes.post("/changePfp", (req: Request, res: Response) => {
+  const gitHubName = req.body.pfp;
+  const user = req.user;
+  const pfpUrl = `https://github.com/${gitHubName}.png`;
+  if (!user) return;
+
+  fetch(pfpUrl).then(async (data) => {
+    try {
+      if (data.status !== 200) throw "We could not find that Github user!";
+
+      await Users.updateOne({ _id: user._id }, { $set: { icon: pfpUrl } });
       res.render("account.ejs", {
         user,
         error: false,
-        success: "Successfully reset your password!",
+        success: false,
         codeError: false,
         pfpError: false,
-        documents: await getDocuments(req.user?.toString(), 10),
+        documents: await getDocuments(user._id.toString(), 10),
       });
     } catch (error) {
       res.render("account.ejs", {
@@ -125,46 +140,15 @@ routes.post("/resetPasswordForm", (req: Request, res: Response) => {
         success: false,
         codeError: false,
         pfpError: false,
-        documents: await getDocuments(req.user?.toString(), 10),
+        documents: await getDocuments(user._id.toString(), 10),
       });
     }
   });
 });
 
-routes.post("/changePfp", (req: Request, res: Response) => {
-  const gitHubName = req.body.pfp;
-  const _id = req.user?.toString();
-  const pfpUrl = `https://github.com/${gitHubName}.png`;
-  fetch(pfpUrl).then((data) => {
-    Users.findOne({ _id }, async (err: string, user: IUser) => {
-      try {
-        if (data.status !== 200) throw "We could not find that Github user!";
-
-        await Users.updateOne({ _id }, { $set: { icon: pfpUrl } });
-        res.render("account.ejs", {
-          user,
-          error: false,
-          success: false,
-          codeError: false,
-          pfpError: false,
-          documents: await getDocuments(req.user?.toString(), 10),
-        });
-      } catch (error) {
-        res.render("account.ejs", {
-          user,
-          error,
-          success: false,
-          codeError: false,
-          pfpError: false,
-          documents: await getDocuments(req.user?.toString(), 10),
-        });
-      }
-    });
-  });
-});
-
-routes.post("/changeDocumentSettings", (req: Request, res: Response) => {
+routes.post("/changeDocumentSettings", async (req: Request, res: Response) => {
   const settings = req.body;
+
   if (
     typeof settings.clipboard !== "boolean" ||
     typeof settings.longerUrls !== "boolean" ||
@@ -179,7 +163,6 @@ routes.post("/changeDocumentSettings", (req: Request, res: Response) => {
         "fuck you trying to give me something else other than booleans and numbers frfr",
     });
 
-  console.log("cum slut");
   const realSettings = {
     clipboard: settings.clipboard || false,
     longerUrls: settings.longerUrls || false,
@@ -188,40 +171,22 @@ routes.post("/changeDocumentSettings", (req: Request, res: Response) => {
     expiration: Math.abs(settings.expiration) || 5,
     imageEmbed: settings.imageEmbed || false,
   };
-  const _id = req.user?.toString();
-  Users.updateOne(
-    { _id },
-    { $set: { settings: realSettings } },
-    {},
-    (err: string) => {
-      if (err)
-        return res.json({
-          success: false,
-          message: "An error occurred whilst saving your settings!",
-        });
-      res.json({
-        success: true,
-        message: "Successfully changed your user settings!",
-      });
-    }
+
+  await Users.updateOne(
+    { _id: req.user?._id.toString() },
+    { $set: { settings: realSettings } }
   );
 });
 
 routes.post("/changePfpGravatar", async (req: Request, res: Response) => {
   const gravatarEmail = req.body.pfp;
-  const _id = req.user?.toString();
+  const user = req.user;
+  const _id = req.user?._id.toString();
   const gravatarUrl = await url(gravatarEmail);
-  const user = await Users.findOne({ _id });
+
   try {
     await Users.updateOne({ _id }, { $set: { icon: gravatarUrl } });
-    res.render("account.ejs", {
-      user,
-      error: false,
-      success: false,
-      codeError: false,
-      pfpError: false,
-      documents: await getDocuments(req.user?.toString(), 10),
-    });
+    res.redirect("/account");
   } catch (err) {
     res.render("account.ejs", {
       user,
@@ -229,47 +194,38 @@ routes.post("/changePfpGravatar", async (req: Request, res: Response) => {
       success: false,
       codeError: false,
       pfpError: "An error has occurred whilst trying to change your pfp!",
-      documents: await getDocuments(req.user?.toString(), 10),
+      documents: await getDocuments(_id, 10),
     });
   }
 });
 
-routes.post("/createInvite", (req: Request, res: Response) => {
-  const _id = req.user?.toString();
-  Users.findOne({ _id }, async (err: string, user: IUser) => {
-    if (user.codesLeft > 0) {
-      await Users.updateOne(
-        { _id },
-        {
-          $set: { codesLeft: user.codesLeft - 1 },
-          // @ts-ignore fuck you typescript
-          $push: { codes: { code: generateString(8) } },
-        }
-      );
+routes.post("/createInvite", async (req: Request, res: Response) => {
+  const user = req.user;
+  if (!user) return;
 
-      res.render("account.ejs", {
-        user,
-        error: false,
-        success: false,
-        codeError: false,
-        pfpError: false,
-        documents: await getDocuments(req.user?.toString(), 10),
-      });
-    } else {
-      res.render("account.ejs", {
-        user,
-        error: false,
-        success: false,
-        codeError: "You've exceeded your max invite count!",
-        pfpError: false,
-        documents: await getDocuments(req.user?.toString(), 10),
-      });
+  if (user.codesLeft < 0)
+    return res.render("account.ejs", {
+      user,
+      error: false,
+      success: false,
+      codeError: "You've exceeded your max invite count!",
+      pfpError: false,
+      documents: await getDocuments(req.user?.toString(), 10),
+    });
+
+  await Users.updateOne(
+    { _id: user._id },
+    {
+      $set: { codesLeft: user.codesLeft - 1 },
+      $push: { "codes.code": generateString(8) },
     }
-  });
+  );
+
+  res.redirect("/account");
 });
 
 routes.get("/createPlusInvite", (req: Request, res: Response) => {
-  if (req.user?.toString() === DEVELOPER_USER) {
+  if (req.user?.admin) {
     const code = signToken(generateString(33));
     res.render("success.ejs", { successMessage: `Plus code: ${code}` });
   } else {
@@ -279,7 +235,7 @@ routes.get("/createPlusInvite", (req: Request, res: Response) => {
 
 routes.post("/updateApiToken", async (req: Request, res: Response) => {
   await Users.updateOne(
-    { _id: req.user?.toString() },
+    { _id: req.user?._id.toString() },
     { $set: { apiToken: createToken() } }
   );
   res.redirect("/account");
