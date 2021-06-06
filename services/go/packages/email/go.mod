8b536850f1b0adf4216612dc6a5f1b0cd1a822bc
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-06 13:22:29 -0400

    some of the email function

---
+++ services/go/packages/email/go.mod
@@ -0,0 +1,8 @@
+module app
+
+go 1.16
+
+require (
+	github.com/aws/aws-sdk-go v1.38.55 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- services/go/packages/email/main.go
+++ services/go/packages/email/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/joho/godotenv"
+)
+
+const (
+	sesRegion = "us-east-1"
+)
+
+/* SES Session */
+
+func SESSession() (*session.Session, error) {
+	return session.NewSession(&aws.Config{
+		Region: aws.String(sesRegion),
+	})
+}
+
+/* Send Email */
+
+func sendEmail(template string, to string, data string) {
+	godotenv.Load()
+
+	config := &aws.Config{
+		Region:      aws.String(sesRegion),
+		Credentials: credentials.NewEnvCredentials(),
+	}
+	session := session.Must(session.NewSession((config)))
+
+	svc := ses.New(session)
+	from := "no-reply@imperialb.in"
+
+	email := &ses.SendTemplatedEmailInput{
+		Source:   &from,
+		Template: &template,
+		Destination: &ses.Destination{
+			ToAddresses: []*string{&to},
+		},
+		TemplateData: &data,
+	}
+
+	svc.SendTemplatedEmail((email))
+}
+
+func testEmail() {
+	data := "{ \"token\":\"" + "test1234" + "\" }"
+	sendEmail("resetPassword", "hello@looskie.com", data)
+}
 
 func main() {
-    fmt.Println("Hello Imperial")
+	testEmail()
 }
