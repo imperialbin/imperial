13314b08023d7d039364e9d657858a35b8518745
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-08 16:38:44 -0400

    SendEmail Utility

---
+++ services/node/packages/app/src/pages/api/sendEmail.ts
@@ -0,0 +1,18 @@
+import { HttpException, api } from "../../util/api";
+import { sendEmail } from "../../util/sendEmail";
+
+export interface HelloResponseType {
+  email: string;
+}
+
+export default api(async (req, res) => {
+  if (Math.random() > 0.7) {
+    throw new HttpException(400, "This was intentionally thrown!");
+  }
+
+  sendEmail("ResetPassword", "hello@looskie.com", "");
+
+  res.json({
+    email: "Sent an email to hello@looskie.com!",
+  });
+});
