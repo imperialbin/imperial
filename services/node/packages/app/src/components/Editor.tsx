6a4180161845a9a341b3ca6dcff4ae6d70438fdc
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-06 00:12:12 -0400

    Cleanup

--- services/node/packages/app/src/components/Editor.tsx
+++ services/node/packages/app/src/components/Editor.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-dynamic(async () => import("ace-builds/src-min-noconflict/theme-imperial"), { ssr: false });
-
 const AceEditor = dynamic(async () => import("react-ace"), { ssr: false });
 
 export const Editor = () => {
