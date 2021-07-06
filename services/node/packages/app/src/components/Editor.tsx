5ea4ad8c37b6552b60b7c202b9bc359bea1a6db2
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-05 23:59:59 -0400

    Feat: Use custom ace builds

--- services/node/packages/app/src/components/Editor.tsx
+++ services/node/packages/app/src/components/Editor.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
+dynamic(async () => import("ace-builds/src-min-noconflict/theme-imperial"), { ssr: false });
+
 const AceEditor = dynamic(async () => import("react-ace"), { ssr: false });
 
 export const Editor = () => {
-  return <AceEditor height="100vh" width="100vw" />;
+  return <AceEditor theme="imperial" height="100vh" width="100vw" />;
 };
