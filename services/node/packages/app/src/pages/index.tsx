64767c7335a516bc8ab7fdb80418b02a072121bf
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-09 14:58:50 -0400

    Chore: Cleanup

--- services/node/packages/app/src/pages/index.tsx
+++ services/node/packages/app/src/pages/index.tsx
@@ -1,17 +1,6 @@
-import { Box, Heading, Kbd } from "@chakra-ui/layout";
-import React, { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-
-// Ambient TypeScript import only
-import type { HelloResponseType } from "./api/hello";
-import { Editor } from "../components/Editor";
+import React from "react";
 import * as imperial from "@imperial/components";
 
 export default function Home() {
-  const [data, setData] = useState<HelloResponseType | null>(null);
-  const [error, setError] = useState<Error | null>(null);
-
-  return (
-      <Editor />
-  );
+  return <imperial.Editor autoFocus />;
 }
