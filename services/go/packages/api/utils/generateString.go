072626ca7cd52230af5b35373845759319b53033
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-10 19:38:23 -0400

    Rename

--- services/go/packages/api/utils/generateString.go
+++ services/go/packages/api/utils/generateString.go
@@ -4,7 +4,7 @@ import "math/rand"
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func RandStringBytes(n int) string {
+func GenerateString(n int) string {
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
