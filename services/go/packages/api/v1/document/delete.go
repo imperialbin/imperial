2e2367cad64fd3a8ea37387c3932528c3093b2bd
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-11 18:00:05 -0400

    Feat: Most of delete done

--- services/go/packages/api/v1/document/delete.go
+++ services/go/packages/api/v1/document/delete.go
@@ -1,7 +1,40 @@
 package document
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"api/prisma/db"
+	"api/utils"
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Delete(c *fiber.Ctx) error {
-	return c.SendString("Deleting deez nuts")
+	var id = c.Params("id")
+	client := utils.GetPrisma()
+	ctx := context.Background()
+
+	deleted, err := client.Document.FindMany(
+		db.Document.DocumentID.Equals(id),
+	).With(
+		db.Document.DocumentSettings.Fetch(),
+	).Delete().Exec(ctx)
+
+	if err != nil {
+		return c.Status(500).JSON(&fiber.Map{
+			"success": false,
+			"message": "An internal server error occurred whilst creating that document!",
+		})
+	}
+
+	if deleted.Count == 0 {
+		return c.Status(404).JSON(&fiber.Map{
+			"success": false,
+			"message": "Document does not exist!",
+		})
+	}
+
+	return c.JSON(&fiber.Map{
+		"success": true,
+		"message": "Document has successfully been deleted!",
+	})
 }
