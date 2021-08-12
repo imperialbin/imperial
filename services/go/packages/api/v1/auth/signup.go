3ef5d16d3a8d285d5b918b967742e102dc5a78cd
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-12 16:49:43 -0400

    Feat: Signup base

--- services/go/packages/api/v1/auth/signup.go
+++ services/go/packages/api/v1/auth/signup.go
@@ -1,7 +1,41 @@
 package auth
 
-import "github.com/gofiber/fiber/v2"
+import (
+	. "api/utils"
+	. "api/v1/commons"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Signup(c *fiber.Ctx) error {
-	return c.SendString("what")
+	req := new(SignupRequest)
+
+	if err := c.BodyParser(req); err != nil {
+		return c.JSON(&fiber.Map{
+			"success": false,
+			"message": "You have a type error in your request!",
+		})
+	}
+
+	errors := ValidateRequest(*req)
+
+	if errors != nil {
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": "You have a validation error in your request",
+			"errors":  errors,
+		})
+	}
+
+	if req.Password != req.ConfirmPassword {
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": "Password does not match confirm password!",
+		})
+	}
+
+	return c.JSON(&fiber.Map{
+		"success": true,
+		"message": "Successfully created your account!",
+	})
 }
