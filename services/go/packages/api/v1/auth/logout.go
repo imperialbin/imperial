a24294088408a17b54bb211d8b8c40d7177c46a3
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-14 18:44:04 -0400

    Finished logout

--- services/go/packages/api/v1/auth/logout.go
+++ services/go/packages/api/v1/auth/logout.go
@@ -1,8 +1,23 @@
 package auth
 
-import "github.com/gofiber/fiber/v2"
+import (
+	. "api/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Logout(c *fiber.Ctx) error {
+	authToken := string(c.Request().Header.Peek("Authentication"))
+
+	_, err := RedisDel(authToken)
+
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"success": false,
+			"message": "There was an internal server error whilst deleting your token!",
+		})
+	}
+
 	return c.JSON(fiber.Map{
 		"success": true,
 		"message": "nut sack",
