e31d5adfb8acb1f6387a38d88cf7244c0c09bff7
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-13 21:09:37 -0400

    Completed login

--- services/go/packages/api/v1/auth/login.go
+++ services/go/packages/api/v1/auth/login.go
@@ -1,7 +1,55 @@
 package auth
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"api/prisma/db"
+	"api/utils"
+	. "api/utils"
+	. "api/v1/commons"
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Login(c *fiber.Ctx) error {
-	return c.SendString("What the the")
+	req := new(LoginRequest)
+	client := utils.GetPrisma()
+	ctx := context.Background()
+
+	if err := c.BodyParser(req); err != nil {
+		return c.JSON(&fiber.Map{
+			"success": false,
+			"message": "You have a type error in your request!",
+		})
+	}
+
+	errors := ValidateRequest(*req)
+
+	if errors != nil {
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": "You have a validation error in your request.",
+			"errors":  errors,
+		})
+	}
+
+	user, err := client.User.FindUnique(
+		db.User.Username.Equals(req.Username),
+	).Exec(ctx)
+
+	if err != nil || !CheckHashedPassword(user.Password, req.Password) {
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": "Username or password is incorrect!",
+		})
+	}
+
+	/* Generate session */
+	token, _ := GenerateSessionToken()
+	RedisSet(token, user.ID, 7)
+
+	return c.JSON(&fiber.Map{
+		"success": true,
+		"message": "Created session",
+		"token":   token,
+	})
 }
