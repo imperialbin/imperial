77fcca81c504ddcc3171fa5e1faedf291ed5865f
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-14 18:15:34 -0400

    Feat: Finish getUser

--- services/go/packages/api/utils/getUser.go
+++ services/go/packages/api/utils/getUser.go
@@ -2,13 +2,14 @@ package utils
 
 import (
 	"api/prisma/db"
+	. "api/v1/commons"
 	"context"
 	"errors"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func GetUser(c *fiber.Ctx) (*db.UserModel, error) {
+func GetUser(c *fiber.Ctx) (*User, error) {
 	authToken := string(c.Request().Header.Peek("Authentication"))
 	userId, ok := RedisGet(authToken)
 
@@ -28,6 +29,38 @@ func GetUser(c *fiber.Ctx) (*db.UserModel, error) {
 		return nil, err
 	}
 
-	return user, nil
+	var formattedUserSettings = UserSettings{
+		user.Settings().Clipboard,
+		user.Settings().LongURLs,
+		user.Settings().ShortURLs,
+		user.Settings().InstantDelete,
+		user.Settings().Encrypted,
+		user.Settings().ImageEmbed,
+		user.Settings().Expiration,
+	}
 
+	var discordId, _ = user.DiscordID()
+	var gitHubAccess, _ = user.GithubAccess()
+	var opt, _ = user.Opt()
+
+	var formattedUser = User{
+		user.ID,
+		user.UserID,
+		user.Username,
+		user.Email,
+		user.Banned,
+		user.Confirmed,
+		user.Icon,
+		user.Password,
+		user.MemberPlus,
+		user.DocumentsMade,
+		user.ActiveUnlimitedDocuments,
+		&discordId,
+		&gitHubAccess,
+		&opt,
+		user.UserSettingsID,
+		formattedUserSettings,
+	}
+
+	return &formattedUser, nil
 }
