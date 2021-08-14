3511b45e9744a1128ce8f96708a72c4b691b10dd
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-14 14:16:03 -0400

    Send data

--- services/go/packages/api/v1/me/me.go
+++ services/go/packages/api/v1/me/me.go
@@ -1,10 +1,22 @@
 package me
 
-import "github.com/gofiber/fiber/v2"
+import (
+	. "api/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Me(c *fiber.Ctx) error {
+	user, err := GetUser(c)
+	if err != nil {
+		return c.Status(403).JSON(&fiber.Map{
+			"success": false,
+			"message": "You are not authorized!",
+		})
+	}
+
 	return c.JSON(&fiber.Map{
 		"success": true,
-		"message": "mate",
+		"data":    user,
 	})
 }
