482eca9b6ca6654bc912845b2526c215046149ec
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-16 18:01:35 -0400

    Feat: added permissions for deleting documents

--- services/go/packages/api/v1/document/delete.go
+++ services/go/packages/api/v1/document/delete.go
@@ -13,13 +13,20 @@ func Delete(c *fiber.Ctx) error {
 	client := GetPrisma()
 	ctx := context.Background()
 
-	deletedDocument, err := client.Document.FindUnique(
-		db.Document.DocumentID.Equals(id),
-	).Delete().Exec(ctx)
+	user, err := GetUser(c)
 
-	client.DocumentSettings.FindUnique(
-		db.DocumentSettings.ID.Equals(deletedDocument.DocumentID),
-	).Delete().Exec(ctx)
+	if err != nil {
+		return c.Status(404).JSON(&fiber.Map{
+			"success": false,
+			"message": "There was an error trying to find your user.",
+		})
+	}
+
+	document, err := client.Document.FindUnique(
+		db.Document.DocumentID.Equals(id),
+	).With(
+		db.Document.DocumentSettings.Fetch(),
+	).Exec(ctx)
 
 	if err != nil {
 		return c.Status(404).JSON(&fiber.Map{
@@ -27,6 +34,24 @@ func Delete(c *fiber.Ctx) error {
 			"message": "Couldn't find document",
 		})
 	}
+
+	documentCreator, _ := document.Creator()
+
+	println(string(documentCreator))
+	if string(documentCreator) != user.Username {
+		return c.Status(401).JSON(&fiber.Map{
+			"success": false,
+			"message": "You do not have access to delete this document!",
+		})
+	}
+
+	deletedDocument, err := client.Document.FindUnique(
+		db.Document.DocumentID.Equals(id),
+	).Delete().Exec(ctx)
+
+	client.DocumentSettings.FindUnique(
+		db.DocumentSettings.ID.Equals(deletedDocument.DocumentID),
+	).Delete().Exec(ctx)
 
 	return c.JSON(&fiber.Map{
 		"success": true,
