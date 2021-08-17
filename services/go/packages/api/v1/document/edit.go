9769cc164fb0b1fff4ed891bb46e5f94cd90a0aa
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-16 21:23:02 -0400

    Most of edit document done

--- services/go/packages/api/v1/document/edit.go
+++ services/go/packages/api/v1/document/edit.go
@@ -1,7 +1,67 @@
 package document
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"api/prisma/db"
+	. "api/utils"
+	. "api/v1/commons"
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Edit(c *fiber.Ctx) error {
+	req := new(EditDocument)
+	client := GetPrisma()
+	ctx := context.Background()
+
+	if err := c.BodyParser(req); err != nil {
+		return c.JSON(&fiber.Map{
+			"success": false,
+			"message": "You have a type error in your request!",
+		})
+	}
+
+	errors := ValidateRequest(*req)
+
+	if errors != nil {
+		return c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": "You have a validation error in your request.",
+			"errors":  errors,
+		})
+	}
+
+	document, err := client.Document.FindFirst(
+		db.Document.DocumentID.Equals(req.Id),
+	).With(
+		db.Document.DocumentSettings.Fetch(),
+	).Exec(ctx)
+
+	if err != nil {
+		return c.Status(404).JSON(&fiber.Map{
+			"success": false,
+			"message": "We couldn't find that document!",
+		})
+	}
+
+	user, err := GetUser(c)
+
+	if err != nil {
+		return c.Status(404).JSON(&fiber.Map{
+			"success": false,
+			"message": "There was an error trying to find your user.",
+		})
+	}
+
+	/* Check if user is in editors and if not yeet them out */
+	documentCreator, _ := document.Creator()
+
+	if user.Username != documentCreator && !ArrayContains(document.DocumentSettings().Editors, user.Username) {
+		return c.Status(401).JSON(&fiber.Map{
+			"success": false,
+			"message": "You are not authorized to edit this document!",
+		})
+	}
+
 	return c.SendString("Editing deez nuts")
 }
