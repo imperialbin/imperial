64e2abe635165ac55e742cddfb943aeda4eb48a9
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-17 14:49:19 -0400

    Feat: get public user

---
+++ services/go/packages/api/v1/user/user.go
@@ -0,0 +1,36 @@
+package user
+
+import (
+	"api/prisma/db"
+	"api/utils"
+	. "api/v1/commons"
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func User(c *fiber.Ctx) error {
+	var username = c.Params("username")
+	client := utils.GetPrisma()
+	ctx := context.Background()
+
+	user, err := client.User.FindUnique(
+		db.User.Username.Equals(username),
+	).Exec(ctx)
+
+	if err != nil {
+		return c.Status(404).JSON(&fiber.Map{
+			"success": false,
+			"message": "We could not find that user!",
+		})
+	}
+
+	return c.JSON(&fiber.Map{
+		"success": true,
+		"data": &PublicUser{
+			Username:   user.Username,
+			Icon:       user.Icon,
+			MemberPlus: user.MemberPlus,
+		},
+	})
+}
