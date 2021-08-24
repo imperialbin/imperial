1c1c3b4a6845dfb7d78867cb12abbdee9f372676
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-23 21:10:34 -0400

    some new types for user!

--- services/node/app/types/index.ts
+++ services/node/app/types/index.ts
@@ -8,3 +8,36 @@ export interface RequestError {
   statusCode: number;
   message: string;
 }
+
+export interface UserResponse {
+  success: boolean;
+  data?: User;
+  message?: string;
+}
+
+export interface User {
+  id: string;
+  userId: number;
+  username: string;
+  email: string;
+  banned: boolean;
+  confirmed: boolean;
+  icon: string;
+  memberPlus: boolean;
+  documentsMade: number;
+  activeUnlimitedDocuments: number;
+  discordId: string;
+  githubAccess: string;
+  opt: string;
+  settings: UserSettings;
+}
+
+export interface UserSettings {
+  clipboard: boolean;
+  longUrls: boolean;
+  shortUrls: boolean;
+  instantDelete: boolean;
+  encrypted: boolean;
+  imageEmbed: boolean;
+  expiration: number;
+}
