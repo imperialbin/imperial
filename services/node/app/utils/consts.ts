80214412d8fa2c864c58882bccf28c4da24ff3f6
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-23 21:10:28 -0400

    Refactor: better consts

--- services/node/app/utils/consts.ts
+++ services/node/app/utils/consts.ts
@@ -1,4 +1,6 @@
 /* ENV and some static stuff */
 
-export const API_URI = "http://localhost:80/";
-export const API_VERSION = "v1";
+export const API_BASE = "http://localhost:80/";
+export const API_VERSION_V1 = "v1";
+
+export const FULL_URI_V1 = API_BASE + API_VERSION_V1;
