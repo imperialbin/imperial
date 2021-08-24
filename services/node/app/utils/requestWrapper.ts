6d96048d476b865a529ac71bb906353d7b585a12
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-23 21:10:08 -0400

    req wrapper

---
+++ services/node/app/utils/requestWrapper.ts
@@ -0,0 +1,24 @@
+import { FULL_URI_V1 } from "./consts";
+
+export const request = async (
+  path: string,
+  method: "POST" | "GET" | "DELETE" | "PATCH",
+  body: any = null
+): Promise<any> => {
+  const request = await fetch(FULL_URI_V1 + path, {
+    method,
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  const data = await request.json();
+
+  if (!request.ok) {
+    return { data: data, error: request.status };
+  }
+
+  return { data: data, error: false };
+};
