fc1c93f92427304950f0814f6c0d8f45610bb444
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-27 02:29:20 -0400

    sleepy

--- services/node/app/components/editor.tsx
+++ services/node/app/components/editor.tsx
@@ -5,16 +5,21 @@ import Monaco, {
   EditorProps,
   DiffEditorProps,
 } from "@monaco-editor/react";
-import React from "react";
+import React, { createRef, useRef } from "react";
 
 import { useAtom } from "jotai";
 import { languageState } from "../state/editor";
 
 export const Editor = (props: EditorProps): JSX.Element => {
   const [language] = useAtom(languageState);
+  const monaco = useRef();
 
   return (
-    <Monaco {...props} language={props.language ? props.language : language} />
+    <Monaco
+      ref={monaco}
+      {...props}
+      language={props.language ? props.language : language}
+    />
   );
 };
 
