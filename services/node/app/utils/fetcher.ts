2a292ada0a1a409ab5971f35b377e7a0850c9230
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-29 19:26:48 -0400

    Refactor: Switch case + default if error doesnt exist

--- services/node/app/utils/fetcher.ts
+++ services/node/app/utils/fetcher.ts
@@ -4,21 +4,27 @@ export const fetcher = async (url: string) => {
     credentials: "include",
   });
 
+  const data = await res.json();
+
   if (!res.ok) {
     let error: any = new Error("An error occurred whilst fetching data!");
 
-    if (res.status === 404)
-      error = new Error("We couldn't find that resource!");
-    if (res.status === 400)
-      error = new Error("You did not provide the correct fields!");
+    switch (res.status) {
+      case 404:
+        error = new Error("We couldn't find that resource!");
+        break;
+      case 500:
+        error = new Error("An internal server error occurred!");
+        break;
 
-    if (res.status >= 500)
-      error = new Error("An internal server error occurred!");
+      default:
+        error = new Error(data.message);
+    }
 
-    error.info = await res.json();
+    error.info = data.message;
     error.status = res.status;
     throw error;
   }
 
-  return res.json();
+  return data;
 };
