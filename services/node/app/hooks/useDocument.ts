c139f3622f69b7829a097e82c00987841735fb54
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-29 19:26:09 -0400

    Feat: Accept password for encrypted documents

--- services/node/app/hooks/useDocument.ts
+++ services/node/app/hooks/useDocument.ts
@@ -1,10 +1,11 @@
 import useSWR from "swr";
-
 import { Document } from "../types";
 import { FULL_URI_V1 } from "../utils/consts";
 
-export const useDocument = (URL: string) => {
-  const { data, error } = useSWR(URL ? FULL_URI_V1 + `/document/${URL}` : null);
+export const useDocument = (URL: string, password: string) => {
+  const { data, error } = useSWR(
+    URL ? FULL_URI_V1 + `/document/${URL}?password=${password}` : null
+  );
   const document: Document = data?.data;
 
   return {
