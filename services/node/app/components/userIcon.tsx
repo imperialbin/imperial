0630174ce7f08a2f452c7d8405ba8eb9a37bd7d3
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-29 20:24:35 -0400

    change initial value to 45 pixels

--- services/node/app/components/userIcon.tsx
+++ services/node/app/components/userIcon.tsx
@@ -8,8 +8,8 @@ const Icon = styled.img`
 
 export const UserIcon = ({
   URL,
-  width = 50,
-  height = 50,
+  width = 45,
+  height = 45,
   style,
 }: UserIconProps): JSX.Element => (
   <Icon width={width} style={style} height={height} src={URL} />
