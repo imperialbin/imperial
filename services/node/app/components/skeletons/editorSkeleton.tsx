07dc81872182cebbf697978b5f8e9bd09849f055
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-30 20:18:09 -0400

    Feat: nicer skeleton + dynamic!

--- services/node/app/components/skeletons/editorSkeleton.tsx
+++ services/node/app/components/skeletons/editorSkeleton.tsx
@@ -2,14 +2,18 @@ import Skeleton from "react-loading-skeleton";
 import styled from "styled-components";
 
 const Container = styled.div`
+  display: flex;
+  flex-direction row;
   width: 100vw;
   height: 100vh;
-  padding: 0 10px;
+  padding: 0 30px;
+  font-size: 14px;
 `;
 
 const CodeContainer = styled.div`
   display: flex;
   flex-direction: column;
+  margin-left: 30px;
 `;
 
 const Lines = styled.div`
@@ -19,6 +23,7 @@ const Lines = styled.div`
 
 const Line = styled.span`
   display: block;
+  margin-top: 3.3px;
 `;
 
 export const EditorSkeleton = (): JSX.Element => {
@@ -34,18 +39,54 @@ export const EditorSkeleton = (): JSX.Element => {
         <Line>7</Line>
         <Line>8</Line>
         <Line>9</Line>
+        <Line>10</Line>
+        <Line>11</Line>
+        <Line>12</Line>
+        <Line>13</Line>
+        <Line>14</Line>
+        <Line>15</Line>
+        <Line>16</Line>
+        <Line>17</Line>
+        <Line>18</Line>
+        <Line>19</Line>
+        <Line>20</Line>
+        <Line>21</Line>
+        <Line>22</Line>
+        <Line>23</Line>
+        <Line>24</Line>
+        <Line>25</Line>
+        <Line>26</Line>
+        <Line>27</Line>
+        <Line>28</Line>
       </Lines>
       <CodeContainer>
-        {[...Array(10)].map((_, i) => {
+        {[...Array(4)].map((_, i) => {
           return (
             <Skeleton
-              style={{ display: "block", marginTop: 2 }}
+              style={{ display: "inline-block", marginTop: 3 }}
               key={i}
-              width={Math.floor(Math.random() * 200) + 50}
+              width={Math.floor(Math.random() * 200) + 100}
               height={16}
             />
           );
         })}
+        <Skeleton style={{ marginTop: 48 }} width={300} height={16} />
+        {[...Array(10)].map((_, i) => {
+          const randomChance = Math.floor(Math.random() * 200);
+          return (
+            <Skeleton
+              style={{
+                display: "block",
+                margin: `${randomChance > 20 ? "3px" : "16px"} 25px`,
+              }}
+              key={i}
+              width={randomChance + 120}
+              height={16}
+            />
+          );
+        })}
+        <Skeleton style={{ marginTop: 3 }} width={30} height={16} />
+        <Skeleton style={{ marginTop: 48 }} width={280} height={16} />
       </CodeContainer>
     </Container>
   );
