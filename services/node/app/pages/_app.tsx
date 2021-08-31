5aec8df048dbf17cbe16ad1f3c3587312ae86a6e
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-08-30 20:25:34 -0400

    Feat: Use ThemeProvider

--- services/node/app/pages/_app.tsx
+++ services/node/app/pages/_app.tsx
@@ -6,22 +6,26 @@ import { fetcher } from "../utils/fetcher";
 import { Provider } from "jotai";
 import store from "../state";
 import { SkeletonTheme } from "react-loading-skeleton";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "../utils/theme";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const { initialState } = pageProps;
   return (
     <Provider initialValues={initialState && [[store.editor, initialState]]}>
-      <SWRConfig
-        value={{
-          fetcher: fetcher,
-          refreshInterval: 120000,
-          revalidateOnFocus: false,
-        }}
-      >
-        <SkeletonTheme color={"#acacac"} highlightColor={"black"}>
-          <Component {...pageProps} />
-        </SkeletonTheme>
-      </SWRConfig>
+      <ThemeProvider theme={darkTheme}>
+        <SWRConfig
+          value={{
+            fetcher: fetcher,
+            refreshInterval: 120000,
+            revalidateOnFocus: false,
+          }}
+        >
+          <SkeletonTheme color={"#acacac"} highlightColor={"black"}>
+            <Component {...pageProps} />
+          </SkeletonTheme>
+        </SWRConfig>
+      </ThemeProvider>
     </Provider>
   );
 }
