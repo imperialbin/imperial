772a7d49b52fd8eceec4d30938f93515ba1f59f4
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-01 12:38:59 -0400

    Fix: Raw Documents are no longer weird

--- services/node/app/pages/r/[id].tsx
+++ services/node/app/pages/r/[id].tsx
@@ -1,15 +1,25 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/dist/client/router";
+import { createGlobalStyle } from "styled-components";
 import { useDocument } from "../../hooks";
 
 const Raw: NextPage = () => {
   const { id, password } = useRouter().query;
   const { document, isError } = useDocument(id as string, password as string);
 
+  const GlobalStyles = createGlobalStyle`
+    body {
+      background: #fff;
+    }
+  `;
+
   return (
-    <span style={{ whiteSpace: "pre-wrap" }}>
-      {isError ? isError.info : document && document.content}
-    </span>
+    <>
+      <GlobalStyles />
+      <span style={{ whiteSpace: "pre-wrap" }}>
+        {isError ? isError.info : document && document.content}
+      </span>
+    </>
   );
 };
 
