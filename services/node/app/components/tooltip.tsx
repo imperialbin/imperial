ebb979563ba1678535f2f9e6d7d6c8866c4b8734
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-03 11:28:51 -0400

    Chore: Cleanup

--- services/node/app/components/tooltip.tsx
+++ services/node/app/components/tooltip.tsx
@@ -1,6 +1,5 @@
 import { ReactChild } from "react";
 import { Tooltip as Tippy, TooltipProps } from "react-tippy";
-import { ThemeForStupidProps } from "../types";
 
 export const Tooltip = (
   props: TooltipProps & { children?: ReactChild }
