42314be95ae3327d295707cfbc565460ffe694f6
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-05 08:39:34 -0400

    Feat: language modal almost done

--- services/node/app/components/modals/languageModal.tsx
+++ services/node/app/components/modals/languageModal.tsx
@@ -1,13 +1,94 @@
 import { useState, useEffect } from "react";
+import { ThemeForStupidProps } from "../../types";
 import { useAtom } from "jotai";
+import styled from "styled-components";
 import { languageState } from "../../state/editor";
 import { activeModal } from "../../state/modal";
 import { supportedLanguages } from "../../utils/consts";
+import { MdFindInPage } from "react-icons/md";
+import { motion } from "framer-motion";
+
+const Search = styled.input`
+  display: block;
+  margin: 15px 0;
+  padding: 10px 0px;
+  border: none;
+  border-radius: 8px;
+  font-size: 1.4em;
+  background: ${({ theme }: ThemeForStupidProps) =>
+    theme.layoutLightestOfTheBunch};
+  color: ${({ theme }: ThemeForStupidProps) => theme.textDarker};
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus::placeholder {
+    color: ${({ theme }: ThemeForStupidProps) => theme.textLight};
+  }
+`;
+
+const LanguageBtn = styled(motion.button)`
+  display: block;
+  width: 100%;
+  text-align: left;
+  font-size: 1.1em;
+  padding: 10px 15px;
+  background: transparent;
+  cursor: pointer;
+  border: none;
+  border-top: 1px solid ${({ theme }: ThemeForStupidProps) => theme.textDarkest};
+  color: ${({ theme }: ThemeForStupidProps) => theme.textLight};
+
+  &:hover {
+    background: ${({ theme }: ThemeForStupidProps) =>
+      theme.layoutLittleLessDark};
+  }
+`;
+
+const UnsupportedLanguage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }: ThemeForStupidProps) => theme.textDarker};
+`;
+
+const Tip = styled.span`
+  position: absolute;
+  left: 10px;
+  bottom: 10px;
+  font-size: 0.9em;
+  opacity: 0.8;
+`;
+
+const TipAccent = styled.span`
+  font-size: 0.9em;
+  font-weight: 700;
+  padding-right: 8px;
+  color: ${({ theme }: ThemeForStupidProps) => theme.success};
+`;
+
+const LanguageBtnAnimation = {
+  initial: {
+    opacity: 0,
+  },
+  isOpen: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+};
 
 export const LanguageModal = (): JSX.Element => {
   const [language, setLanguage] = useAtom(languageState);
   const [searchInput, setSearchInput] = useState<string>("");
   const [, setActiveModal] = useAtom(activeModal);
+
+  const languageFilter = supportedLanguages.filter((language) =>
+    language.name.startsWith(searchInput)
+  );
 
   const changeLanguage = (language: string) => {
     setActiveModal([null, null]);
@@ -33,27 +114,36 @@ export const LanguageModal = (): JSX.Element => {
   return (
     <>
       Selected language: {language}
-      <input
+      <Search
         placeholder="Search languages"
         onChange={(e) => setSearchInput(e.target.value)}
         autoFocus
       />
-      <ul>
-        {supportedLanguages
-          .filter((language) => language.name.startsWith(searchInput))
-          .map((language, key) => {
-            return (
-              <button
-                style={{ display: "block" }}
-                onClick={() => changeLanguage(language.name)}
-                key={key}
-                value={language.name}
-              >
-                {language.name}
-              </button>
-            );
-          })}
-      </ul>
+      {languageFilter.length === 0 && (
+        <UnsupportedLanguage>
+          <MdFindInPage size={50} />
+          <p>We don&apos;t support that language</p>
+        </UnsupportedLanguage>
+      )}
+      {languageFilter.map((language, key) => {
+        return (
+          <LanguageBtn
+            transition={{ duration: 0.2 }}
+            variants={LanguageBtnAnimation}
+            onClick={() => changeLanguage(language.name)}
+            key={key}
+            value={language.name}
+          >
+            {language.name}
+          </LanguageBtn>
+        );
+      })}
+      {searchInput && (
+        <Tip>
+          <TipAccent>PROTIP</TipAccent>
+          you can press enter to select the first result.
+        </Tip>
+      )}
     </>
   );
 };
