c930e7a3d930351dbe70062414e9c1a2e3dd06d0
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-06 21:37:49 -0400

    Refactor: use styled component Anchor

--- services/node/app/components/tooltips/loggedIn.tsx
+++ services/node/app/components/tooltips/loggedIn.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { DiscordURL, GitHubURL } from "../../utils/consts";
-import { ListContainer, ListItem, Seperator } from "./styles";
+import { Anchor, ListContainer, ListItem, Seperator } from "./styles";
 
 export const LoggedInTooltip = (): JSX.Element => {
   return (
@@ -8,13 +8,13 @@ export const LoggedInTooltip = (): JSX.Element => {
       <Link href="/account">
         <ListItem>User settings</ListItem>
       </Link>
-      <Link href={DiscordURL}>
-        <ListItem>Discord</ListItem>
-      </Link>
+      <Anchor href={DiscordURL} target="_blank" rel="noreferrer">
+        Discord
+      </Anchor>
       <Seperator />
-      <Link href={GitHubURL}>
-        <ListItem>GitHub</ListItem>
-      </Link>
+      <Anchor href={GitHubURL} target="_blank" rel="noreferrer">
+        GitHub
+      </Anchor>
       <Link href="/logout">
         <ListItem>Logout</ListItem>
       </Link>
