586451a4d00793a104affff955c789395a99d234
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-07 17:17:25 -0400

    Fix: Logout route

--- services/node/app/pages/logout.tsx
+++ services/node/app/pages/logout.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import type { NextPage } from "next";
+import Router from "next/router";
 import { AuthError } from "../components/401";
 import { useUser } from "../hooks/useUser";
 import { request } from "../utils/requestWrapper";
@@ -6,16 +8,15 @@ import { request } from "../utils/requestWrapper";
 const Logout: NextPage = () => {
   const { user, isError, isLoading } = useUser();
 
-  if (!user && !isLoading) return <AuthError />;
+  useEffect(() => {
+    if (!user && !isLoading) Router.replace("/login");
+  }, [user, isLoading]);
 
   request("/auth/logout", "DELETE");
 
-  return (
-    <h1>
-      Logging out i actually didnt finish this lol but if it doesnt error you
-      shouldnt have a valid session anymore
-    </h1>
-  );
+  Router.replace("/login");
+
+  return user ? <h1>Destroying session...</h1> : <h1></h1>;
 };
 
 export default Logout;
