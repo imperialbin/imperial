b5e891b48481bf4abe7bdd841cc1a3d7d67ba592
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-10-06 17:44:35 -0400

    Refactor

--- services/node/app/state/modal/modals.ts
+++ services/node/app/state/modal/modals.ts
@@ -1,20 +1,30 @@
 export const modals = {
   language: {
     title: "Select a language",
+    noHeader: false,
   },
   addUsers: {
     title: "Add users",
+    noHeader: false,
   },
   userSettings: {
     title: "User settings",
+    noHeader: true,
   },
   documentSettings: {
     title: "Document settings",
+    noHeader: false,
   },
   login: {
     title: "Welcome back!",
+    noHeader: true,
   },
   signup: {
     title: "Welcome aboard!",
+    noHeader: true,
+  },
+  resetPassword: {
+    title: "Reset password",
+    noHeader: true,
   },
 };
