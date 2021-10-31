9b8389e3939fe3009507b74ca96ee039f66955d8
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-10-31 18:33:42 -0400

    oops

--- services/node/app/pages/index.tsx
+++ services/node/app/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAtom } from "jotai";
 import type { NextPage } from "next";
 import { Editor } from "../components/ui";
