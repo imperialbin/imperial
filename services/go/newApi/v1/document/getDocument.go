c78603d2d284a0d556c90dbaf14ae990fdb6edaa
Cody Miller <hello@looskie.com>
2022-06-13 23:48:18 -0400

    finished get document

--- services/go/newApi/v1/document/getDocument.go
+++ services/go/newApi/v1/document/getDocument.go
@@ -1,15 +1,37 @@
 package document
 
 import (
+	"errors"
+	"newapi/models"
+	"newapi/utils"
 	. "newapi/v1/commons"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 func Get(c *fiber.Ctx) error {
+	var id = c.Params("id")
+
+	client := utils.GetDB()
+
+	var document models.Document
+	if result := client.First(&document, "id = ?", id); result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return c.Status(404).JSON(Response{
+				Success: false,
+				Message: "No document with that ID exists.",
+			})
+		}
+
+		return c.Status(500).JSON(Response{
+			Success: false,
+			Message: "An internal server error occurred :(",
+		})
+	}
+
 	return c.JSON(Response{
 		Success: true,
-
-		Message: "yo",
+		Data:    document,
 	})
 }
