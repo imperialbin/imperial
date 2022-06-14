afb44e89b4bd0c239d8ce1a64050edf94d39fc20
Cody Miller <hello@looskie.com>
2022-06-13 23:48:00 -0400

    get user

--- services/go/newApi/v1/users/getUser.go
+++ services/go/newApi/v1/users/getUser.go
@@ -1,15 +1,26 @@
 package users
 
 import (
+	"newapi/utils"
 	. "newapi/v1/commons"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func FindUser(c *fiber.Ctx) error {
+	var username = c.Params("username")
+
+	user, err := utils.GetUserPartial(username)
+
+	if err != nil {
+		return c.Status(500).JSON(Response{
+			Success: false,
+			Message: err.Error(),
+		})
+	}
 
 	return c.JSON(Response{
 		Success: true,
-		Message: "yo",
+		Data:    user,
 	})
 }
