7ea34d0746e41dc08f9a78a8754c18f05c849921
Cody Miller <hello@looskie.com>
2022-06-14 19:39:52 -0400

    feat: getUserPartialByID

--- services/go/newApi/utils/getUserPartial.go
+++ services/go/newApi/utils/getUserPartial.go
@@ -18,3 +18,20 @@ func GetUserPartial(username string) (*models.UserPartial, error) {
 		Flags:         user.Flags,
 	}, nil
 }
+
+func GetUserPartialByID(ID uint) (*models.UserPartial, error) {
+	client := GetDB()
+
+	var user models.User
+	if result := client.First(&user, "id = ? ", ID); result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &models.UserPartial{
+		UserID:        user.ID,
+		Username:      user.Username,
+		DocumentsMade: user.DocumentsMade,
+		Icon:          user.Icon,
+		Flags:         user.Flags,
+	}, nil
+}
