6411b61d04c139bbbd0dadcad6cdd8855eacb7f2
Cody Miller <hello@looskie.com>
2022-06-15 16:12:27 -0400

    bruh

--- services/node/old_app/next.config.js
+++ services/node/old_app/next.config.js
@@ -2,6 +2,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     domains: [
       "localhost",
