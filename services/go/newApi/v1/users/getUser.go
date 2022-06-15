7f89df12959a84e413e52ec1c8199e42af191f05
Cody Miller <hello@looskie.com>
2022-06-15 03:16:54 -0400

    feat: better error message

--- services/go/newApi/v1/users/getUser.go
+++ services/go/newApi/v1/users/getUser.go
@@ -1,10 +1,12 @@
 package users
 
 import (
+	"errors"
 	"newapi/utils"
 	. "newapi/v1/commons"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 func FindUser(c *fiber.Ctx) error {
@@ -13,6 +15,13 @@ func FindUser(c *fiber.Ctx) error {
 	user, err := utils.GetUserPartial(username)
 
 	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return c.Status(404).JSON(Response{
+				Success: false,
+				Message: "User not found.",
+			})
+		}
+
 		return c.Status(500).JSON(Response{
 			Success: false,
 			Message: err.Error(),
