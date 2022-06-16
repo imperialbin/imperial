51dc7bd4e3c2c3279b805563fa3c7a4a54598adf
Cody Miller <hello@looskie.com>
2022-06-16 10:20:56 -0400

    feat: dynamically add and check for api tokens in redis

--- services/go/api/utils/getAuthedUser.go
+++ services/go/api/utils/getAuthedUser.go
@@ -3,7 +3,10 @@ package utils
 import (
 	"api/models"
 	"errors"
+	"fmt"
+	"strings"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
@@ -11,12 +14,27 @@ import (
 func GetAuthedUser(c *fiber.Ctx) (*models.User, error) {
 	authToken := GetAuthToken(c)
 	userID, err := RedisGet(authToken)
+	var client = GetDB()
 
+	// Dear lord help me as i've commited a sin and nested a bunch of ifs together
 	if err != nil {
-		return nil, nil
-	}
+		// if request is an API token
+		if !strings.Contains(authToken, "IMPERIAL-AUTH") {
+			var apiToken models.User
+			if result := client.First(&apiToken, "api_token = ? ", authToken).Select("api_token"); result.Error != nil {
+				println(err.Error())
+				if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+					return nil, nil
+				}
 
-	client := GetDB()
+				sentry.CaptureException(err)
+				return nil, nil
+			}
+
+			RedisSet(apiToken.APIToken, fmt.Sprint(apiToken.ID), -1)
+			userID = fmt.Sprint(apiToken.ID)
+		}
+	}
 
 	var user models.User
 	if result := client.Preload("UserSettings").First(&user, "id = ? ", userID); result.Error != nil {
