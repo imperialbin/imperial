dfc96f7ea646a74a339732fa8c3b65c96a3000d8
Cody Miller <hello@looskie.com>
2022-06-17 04:03:45 -0400

    devex: types + notification actions

--- services/node/app/state/actions/index.ts
+++ services/node/app/state/actions/index.ts
@@ -1,5 +1,6 @@
-import { Modals } from "../../src/components/ModalManager";
+import { ModalData, Modals } from "../../src/components/ModalManager";
 import { SelfUser } from "../../src/types";
+import { Notification } from "../reducers/notifications";
 
 export const setUser = (user: SelfUser) => {
   return {
@@ -8,7 +9,7 @@ export const setUser = (user: SelfUser) => {
   };
 };
 
-export const openModal = (modal: Modals, data?: unknown) => {
+export const openModal = <T extends Modals>(modal: T, data?: ModalData<T>) => {
   return {
     type: "OPEN_MODAL",
     payload: { modal, data },
@@ -18,5 +19,26 @@ export const openModal = (modal: Modals, data?: unknown) => {
 export const closeModal = () => {
   return {
     type: "CLOSE_MODAL",
+  };
+};
+
+export const addNotification = (notification: Omit<Notification, "id">) => {
+  return {
+    type: "ADD_NOTIFICATION",
+    payload: notification,
+  };
+};
+
+export const removeNotification = (id: string) => {
+  console.log(id);
+  return {
+    type: "REMOVE_NOTIFICATION",
+    payload: { id },
+  };
+};
+
+export const removeAllNotifications = () => {
+  return {
+    type: "REMOVE_ALL_NOTIFICATION",
   };
 };
