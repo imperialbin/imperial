507599a83c48d4c52547a0f2187b3a3865f1e668
Cody Miller <hello@looskie.com>
2022-06-17 05:28:59 -0400

    fix: allow user icons to be cool

--- services/node/app/next.config.js
+++ services/node/app/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "localhost",
+      "github.com",
+      "gravatar.com",
+      "avatars.githubusercontent.com",
+    ],
+  },
 };
 
 module.exports = nextConfig;
