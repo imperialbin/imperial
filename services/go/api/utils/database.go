c95bd0b5c709b5adaa72a694340deb5db42963e4
Cody Miller <hello@looskie.com>
2022-08-31 23:39:45 -0400

    Update database.go

--- services/go/api/utils/database.go
+++ services/go/api/utils/database.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"api/models"
 	"os"
-	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,8 +12,6 @@ var globalDB *gorm.DB
 
 func InitDB() *gorm.DB {
 	var dbURL = os.Getenv("DATABASE_URL")
-
-	time.Sleep(150 * time.Millisecond)
 
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{
 		FullSaveAssociations: true,
