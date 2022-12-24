74800c703de5b82dd03b2395d3b0000253da2c24
Cody Miller <hello@looskie.com>
2022-12-24 00:52:27 -0500

    fix: no found users message

--- services/go/api/v1/users/search_users.go
+++ services/go/api/v1/users/search_users.go
@@ -4,10 +4,12 @@ import (
 	"api/models"
 	"api/utils"
 	. "api/v1/commons"
+	"errors"
 	"strings"
 
 	"github.com/getsentry/sentry-go"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 func SearchUsers(c *fiber.Ctx) error {
@@ -19,9 +21,23 @@ func SearchUsers(c *fiber.Ctx) error {
 	if result := client.Find(&users, "username LIKE ? ", "%"+strings.ToLower(username)+"%"); result.Error != nil {
 		sentry.CaptureException(result.Error)
 
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return c.Status(404).JSON(Response{
+				Success: false,
+				Message: "No users found",
+			})
+		}
+
 		return c.Status(500).JSON(Response{
 			Success: false,
 			Message: "An internal server error occurred",
+		})
+	}
+
+	if len(users) == 0 {
+		return c.Status(404).JSON(Response{
+			Success: false,
+			Message: "No users found",
 		})
 	}
 
