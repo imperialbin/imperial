7e7fed1e2ac013e57436338bb5e717dbb89507e4
Cody Miller <hello@looskie.com>
2022-12-25 10:53:57 -0500

    devex: sentry

---
+++ services/node/web/src/app/ErrorBoundary.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Button from "../components/Button";
+import { Logo } from "../components/Icons";
+import { styled } from "../stitches";
+
+const Container = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+  width: "100vw",
+  color: "$text-muted",
+
+  "> svg": {
+    position: "absolute",
+    top: 35,
+    left: 35,
+    height: 80,
+    width: 80,
+  },
+});
+
+const Title = styled("h1", {
+  fontWeight: "bolder",
+  fontSize: "5.5em",
+  fontFamily: "$mono",
+  color: "$text-primary",
+});
+
+const Paragraph = styled("p", {
+  fontSize: "1.5em",
+  maxWidth: "45ch",
+  textAlign: "center",
+  fontFamily: "$mono",
+  color: "$text-secondary",
+  marginBottom: 20,
+});
+
+class ErrorBoundary extends React.Component<
+  {
+    children: React.ReactNode;
+  },
+  { error: boolean }
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = { error: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { error: true };
+  }
+
+  render() {
+    if (this.state.error) {
+      return (
+        <Container>
+          <Logo />
+          <Title>Oops</Title>
+          <Paragraph>
+            You've encountered a client error. No worries though! We've sent the
+            error to our developers.
+          </Paragraph>
+          <Button onClick={() => window.location.reload()}>Reload</Button>
+          <span style={{ position: "absolute", bottom: 35 }}>© IMPERIAL</span>
+        </Container>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- services/node/web/src/main.tsx
+++ services/node/web/src/main.tsx
@@ -2,28 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { SkeletonTheme } from "react-loading-skeleton";
 import { Provider } from "react-redux";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 import { SWRConfig } from "swr";
 import App from "./App";
 import { store } from "./state";
 import { fetcher } from "./utils/Rest";
 
 import "react-loading-skeleton/dist/skeleton.css";
+import ErrorBoundary from "./app/ErrorBoundary";
+
+Sentry.init({
+  dsn: "https://25ed4413241f432b8c56111756d4d886@o718150.ingest.sentry.io/5780575",
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <SWRConfig
-        value={{
-          fetcher: (url: string) => fetcher("GET", url),
-        }}
-      >
-        <SkeletonTheme
-          baseColor="var(--bg-contrast)"
-          highlightColor="var(--bg-secondary)"
+    <ErrorBoundary>
+      <Provider store={store}>
+        <SWRConfig
+          value={{
+            fetcher: (url: string) => fetcher("GET", url),
+          }}
         >
-          <App />
-        </SkeletonTheme>
-      </SWRConfig>
-    </Provider>
+          <SkeletonTheme
+            baseColor="var(--bg-contrast)"
+            highlightColor="var(--bg-secondary)"
+          >
+            <App />
+          </SkeletonTheme>
+        </SWRConfig>
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
