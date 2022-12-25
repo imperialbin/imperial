01f63284cce0dc84e9999287829e482b840c940c
Cody Miller <hello@looskie.com>
2022-12-24 23:10:50 -0500

    rename param

--- services/go/api/utils/screenshot_documents.go
+++ services/go/api/utils/screenshot_documents.go
@@ -31,9 +31,9 @@ func ScreenshotDocument(documentID string, userFlag int) {
 	SaveImage(documentID, buf)
 }
 
-func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
+func fullScreenshot(url string, quality int, res *[]byte) chromedp.Tasks {
 	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
+		chromedp.Navigate(url),
 		chromedp.WaitVisible("div.monaco-editor"),
 		chromedp.FullScreenshot(res, quality),
 	}
