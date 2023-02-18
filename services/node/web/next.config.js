70d8570e03e45bdd9730d7e876ed5fa2c584e088
Cody Miller <hello@looskie.com>
2023-02-18 11:01:19 -0500

    redirects

--- services/node/web/next.config.js
+++ services/node/web/next.config.js
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/cTm85eW49D",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/imperialbin/",
+        permanent: true,
+      },
+      {
+        source: "/link/discord",
+        destination:
+          process.env.NODE_ENV !== "development"
+            ? "http://localhost:8080/v1/oauth/discord"
+            : "https://imperial.hop.sh/v1/oauth/discord",
+        permanent: true,
+      },
+      {
+        source: "/link/github",
+        destination:
+          process.env.NODE_ENV !== "development"
+            ? "http://localhost:8080/v1/oauth/github"
+            : "https://imperial.hop.sh/v1/oauth/github",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
