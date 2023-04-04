486b80329767616ebb37908575e67cca8b6cc3c4
Cody Miller <hello@looskie.com>
2023-04-04 19:57:29 -0400

    migrate: to znv

--- apps/web/src/utils/Env.ts
+++ apps/web/src/utils/Env.ts
@@ -1,37 +1,13 @@
-import { bool, envsafe, num, str } from "envsafe";
+import { parseEnv } from "znv";
+import { z } from "zod";
 
-export const env = envsafe({
-  PRODUCTION: bool({
-    default: false,
-    desc: "Whether the website is running in production",
-  }),
-  API_URL_V1: str({
-    default: "http://127.0.0.1:8080/v1",
-    desc: "The api url v1",
-  }),
-  CDN_URL: str({
-    default: "https://cdn.impb.in/",
-    desc: "The cdn url",
-  }),
-  DISCORD_INVITE: str({
-    default: "https://discord.gg/cTm85eW49D",
-    desc: "The discord invite url",
-  }),
-  GITHUB_URL: str({
-    default: "https://github.com/imperialbin",
-    desc: "The github url",
-  }),
-  TWITTER_URL: str({
-    default: "https://twitter.com/imperialbin",
-    desc: "The twitter url",
-  }),
-  SENTRY_DSN: str({
-    default: "",
-    desc: "The sentry dsn",
-    allowEmpty: true,
-  }),
-  SENTRY_ENVIRONMENT: str({
-    default: "Local",
-    desc: "The sentry environment",
-  }),
+export const env = parseEnv(process.env, {
+  PRODUCTION: z.boolean().default(false),
+  API_URL_V1: z.string().default("http://127.0.0.1:8080/v1"),
+  CDN_URL: z.string().default("https://cdn.impb.in/"),
+  DISCORD_INVITE: z.string().default("https://discord.gg/cTm85eW49D"),
+  GITHUB_URL: z.string().default("https://github.com/imperialbin"),
+  TWITTER_URL: z.string().default("https://twitter.com/imperialbin"),
+  SENTRY_DSN: z.string().default(""),
+  SENTRY_ENVIRONMENT: z.string().default(""),
 });
