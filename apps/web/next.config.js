b84f929396936aee36d4c7eaeea9ea4409d54724
Cody Miller <hello@looskie.com>
2023-04-04 12:48:56 -0400

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup please dpeloy/
 const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
