aea397d9b9bff6d2a83a1c61ed314db2cec4b4f7
Cody Miller <hello@looskie.com>
2023-04-04 20:17:22 -0400

    rm production var

--- apps/web/src/utils/Env.ts
+++ apps/web/src/utils/Env.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 const env_schema = z.object({
-  PRODUCTION: z.string().default(false),
   API_URL_V1: z.string().default("http://127.0.0.1:8080/v1"),
   CDN_URL: z.string().default("https://cdn.impb.in/"),
   DISCORD_INVITE: z.string().default("https://discord.gg/cTm85eW49D"),
