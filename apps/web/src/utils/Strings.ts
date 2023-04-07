cda1ffa3e4ef63ff2cdd98efb582b586e499848f
Cody Miller <hello@looskie.com>
2023-04-07 09:23:27 -0400

    Update Strings.ts

--- apps/web/src/utils/Strings.ts
+++ apps/web/src/utils/Strings.ts
@@ -1,2 +1,2 @@
-/* Not cryuptographically safe, but mostly random string */
+/* Not cryptographically safe, but mostly random string */
 export const generateString = () => (Math.random() + 1).toString(36).substring(7);
