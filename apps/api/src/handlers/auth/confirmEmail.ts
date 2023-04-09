95896da63a4e8dd7711b072ee5378b8c1f9beeda
Cody Miller <hello@looskie.com>
2023-04-09 11:28:53 -0400

    refactor: accept token in body instead of query string

--- apps/api/src/handlers/auth/confirmEmail.ts
+++ apps/api/src/handlers/auth/confirmEmail.ts
@@ -1,18 +1,32 @@
 import { Id } from "@imperial/commons";
 import { eq } from "drizzle-orm/expressions";
+import { z } from "zod";
 import { db } from "../../db";
 import { users } from "../../db/schemas";
 import { FastifyImp } from "../../types";
 import { Redis } from "../../utils/redis";
 
-export const confirmEmail: FastifyImp<{
-  Querystring: {
-    token: string;
-  };
-}> = async (request, reply) => {
-  const { token } = request.query;
+const confirmEmailBody = z.object({
+  token: z.string().length(32),
+});
 
-  const maybeUserId = await Redis.get<Id<"user">>("confirm_email_token", token);
+export const confirmEmail: FastifyImp<{
+  Body: z.infer<typeof confirmEmailBody>;
+}> = async (request, reply) => {
+  const body = confirmEmailBody.safeParse(request.body);
+  if (!body.success) {
+    return reply.status(400).send({
+      success: false,
+      error: {
+        message: body.error.message,
+      },
+    });
+  }
+
+  const maybeUserId = await Redis.get<Id<"user">>(
+    "confirm_email_token",
+    body.data.token,
+  );
   if (!maybeUserId) {
     return reply.status(400).send({
       success: false,
