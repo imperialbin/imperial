96b9e9a85b560ab8b374d1a470c9d2a6624465fc
Cody Miller <50378828+Looskie@users.noreply.github.com>
2023-04-11 20:26:19 -0400

    fix: run on every sunday midnight

--- apps/api/src/utils/crons.ts
+++ apps/api/src/utils/crons.ts
@@ -4,7 +4,7 @@ import { db } from "../db";
 import { documents } from "../db/schemas";
 import { Logger } from "./logger";
 
-export const deleteExpiredDocuments = new CronJob("* * * * *", async () => {
+export const deleteExpiredDocuments = new CronJob("0 0 * * SUN", async () => {
   const expiredDocuments = await db
     .delete(documents)
     .where(sql`${documents.expires_at}::date < NOW()::date`)
