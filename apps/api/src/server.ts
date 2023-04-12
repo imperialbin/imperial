448535cad4022ece8a39099ac00d3f03fbe357f8
Cody Miller <50378828+Looskie@users.noreply.github.com>
2023-04-11 20:25:59 -0400

    refactor: move server code into sevrer.ts and inits into index.ts

---
+++ apps/api/src/server.ts
@@ -0,0 +1,118 @@
+import cors from "@fastify/cors";
+import * as Sentry from "@sentry/node";
+import fastify from "fastify";
+import { setupDB } from "./db";
+import { middleware } from "./modules/middleware";
+import { adminRoutes } from "./routes/admin";
+import { authRoutes } from "./routes/auth";
+import { documentRoutes } from "./routes/document";
+import { oAuthRoutes } from "./routes/oauth";
+import { usersRoutes } from "./routes/users";
+import { env } from "./utils/env";
+import { Logger } from "./utils/logger";
+import { Redis } from "./utils/redis";
+
+Sentry.init({
+  dsn: env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  environment: env.SENTRY_ENVIRONMENT,
+});
+
+export const main = async () => {
+  const server = fastify({
+    bodyLimit: 5000000,
+    trustProxy: true,
+  });
+
+  await setupDB();
+  await Redis.initialize();
+
+  const API_VERSION = "v1";
+
+  server.register(middleware);
+  server.register(cors, { maxAge: 600, origin: true, credentials: true });
+  server.register(import("@fastify/cookie"), {
+    secret: env.COOKIE_SIGNER,
+    parseOptions: {
+      httpOnly: true,
+      secure: env.PRODUCTION,
+      sameSite: env.PRODUCTION ? "lax" : "none",
+      path: "/",
+      domain: ".impb.in",
+      // New date 6 months from now
+      expires: new Date(Date.now() + 15778476000),
+    },
+  });
+  await server.register(import("@fastify/rate-limit"), {
+    global: true,
+    max: 500,
+    timeWindow: "1 minute",
+    keyGenerator(request) {
+      return (request.headers["x-real-ip"] ??
+        request.headers["x-client-ip"] ??
+        request.headers["x-forwarded-for"] ??
+        request.ip) as string;
+    },
+  });
+
+  server.get("/", async (_, reply) => {
+    reply.send({
+      success: true,
+      data: {
+        message: "Welcome to Imperial API",
+      },
+    });
+  });
+  server.get("/v1", async (_, reply) => {
+    reply.send({
+      success: true,
+      data: {
+        message: "Welcome to Imperial API",
+        version: "1",
+        documentation: "https://docs.imperialb.in/v1",
+      },
+    });
+  });
+  server.register(usersRoutes, { prefix: `/${API_VERSION}/users` });
+  server.register(documentRoutes, { prefix: `/${API_VERSION}/document` });
+  server.register(authRoutes, { prefix: `/${API_VERSION}/auth` });
+  server.register(oAuthRoutes, { prefix: `/${API_VERSION}/oauth` });
+  server.register(adminRoutes, { prefix: `/${API_VERSION}/admin` });
+
+  server.setNotFoundHandler((_, reply) => {
+    reply.code(404).send({
+      success: false,
+      error: {
+        message: "Route not found",
+      },
+    });
+  });
+
+  server.setErrorHandler((error, _, reply) => {
+    if ((error.statusCode ?? 500) >= 500) {
+      Logger.error("SERVER", error.message);
+      Sentry.captureException(error);
+    }
+
+    reply.code(error.statusCode ?? 500).send({
+      success: false,
+      error: {
+        message:
+          (error.statusCode ?? 500) >= 500
+            ? "Internal server error"
+            : error.message,
+      },
+    });
+  });
+
+  server.listen({ host: env.HOST, port: env.PORT }, (err, address) => {
+    if (err) {
+      Logger.error("INIT", err.message);
+      process.exit(1);
+    }
+
+    Logger.info("INIT", `Server listening at ${address}`);
+  });
+
+  return server;
+};
