99fcf457dbc9ac36e68627334bd52e60f8cbe6ce
Cody Miller <50378828+Looskie@users.noreply.github.com>
2023-04-14 10:03:12 -0400

    woah there bud

--- apps/web/src/components/modals/LanguageSelectorModal.tsx
+++ apps/web/src/components/modals/LanguageSelectorModal.tsx
@@ -73,7 +73,7 @@ export default function LanguageSelector({ closeModal, dispatch }: ModalProps) {
     return () => {
       window.removeEventListener("keydown", enterHandler);
     };
-  });
+  }, []);
 
   const filteredLanguages = useMemo(
     () =>
