900706c42441044803ca4de1f6a081e18573c87c
Cody Miller <hello@looskie.com>
2023-04-30 20:50:18 -0400

    seo probs

--- apps/web/src/next-seo.config.ts
+++ apps/web/src/next-seo.config.ts
@@ -8,12 +8,18 @@ const config: DefaultSeoProps = {
     url: "https://impb.in/",
     siteName: "IMPERIAL",
     title: "IMPERIAL",
+    images: [
+      {
+        url: "https://cdn.impb.in/assets/header.png",
+      },
+    ],
   },
   twitter: {
     handle: "@imperialbin",
     site: "@imperial",
     cardType: "summary_large_image",
   },
+  description: "Share code with anyone in a matter of seconds.",
 };
 
 export default config;
