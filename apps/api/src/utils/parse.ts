dbd1adeb1d82fbe5fd0a3bd94e1d61e90bff09b8
Cody Miller <hello@looskie.com>
2023-05-26 11:54:21 -0400

    catch if it breaks

--- apps/api/src/utils/parse.ts
+++ apps/api/src/utils/parse.ts
@@ -1,8 +1,12 @@
 const parseDomainFromOrigin = (origin: string) => {
-  const url = new URL(origin);
-  const domain = url.hostname.split(".").slice(-2).join(".");
+  try {
+    const url = new URL(origin);
+    const domain = url.hostname.split(".").slice(-2).join(".");
 
-  return domain;
+    return domain;
+  } catch {
+    return "imperialb.in";
+  }
 };
 
 export { parseDomainFromOrigin };
