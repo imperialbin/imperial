500c4dc4e28a9005e0d1bccd29ecbe9780ec077b
Cody Miller <hello@looskie.com>
2023-05-27 12:54:58 -0400

    api (feat): new payment emails

---
+++ apps/api/src/utils/emails/order_refunded.tsx
@@ -0,0 +1,23 @@
+import PageBase from "./components/PageBase";
+import React from "react";
+import Header from "./components/Header";
+import Button from "./components/Button";
+import { EmailProps } from "./emails";
+
+export default function OrderRefunded({
+  receipt_url,
+  amount_refunded,
+}: EmailProps["order_refunded"]) {
+  return (
+    <PageBase title="Order Refunded">
+      <Header header="Order Refunded" subHeader="BILLING" />
+      <p>
+        We've refunded your order, you should receive ${amount_refunded} in your
+        account within 5-10 business days. Furthermore, the Member+ token will
+        be revoked and removed from any user that used it. If you have any
+        questions, please contact us on our Discord or Twitter!
+      </p>
+      <Button href={receipt_url}>Receipt</Button>
+    </PageBase>
+  );
+}
