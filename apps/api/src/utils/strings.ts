7530f46d44b681239a12ae446af24c17ce5411a2
Cody Miller <hello@looskie.com>
2023-05-26 22:36:06 -0400

    api (fix): generateRandomSecureString should respect lengths

--- apps/api/src/utils/strings.ts
+++ apps/api/src/utils/strings.ts
@@ -2,7 +2,7 @@ import { randomBytes } from "crypto";
 import { customAlphabet } from "nanoid";
 
 const generateRandomSecureString = (length: number): string =>
-  randomBytes(length).toString("hex");
+  randomBytes(256).toString("hex").slice(0, length);
 
 // Alphabet minus l and i because thats annoying trying to figure out the difference between them
 const documentIdGenerator = customAlphabet("abcdefghjknompqrstuvwxyz0", 8);
