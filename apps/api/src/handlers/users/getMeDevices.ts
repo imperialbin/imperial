900e892104a47b91b64849130e37c701d27bb076
Cody Miller <hello@looskie.com>
2023-05-31 18:50:11 -0400

    api (feat): add this_device tag to getMeDevices

--- apps/api/src/handlers/users/getMeDevices.ts
+++ apps/api/src/handlers/users/getMeDevices.ts
@@ -1,15 +1,20 @@
-import { eq } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import { db } from "../../db";
 import { devices } from "../../db/schemas";
 import { FastifyImp } from "../../types";
 import { DEVICES_PUBLIC_OBJECT } from "../../utils/publicObjects";
+import { Device } from "@imperial/commons";
 
-export const getMeDevices: FastifyImp<{}, unknown, true> = async (
+export const getMeDevices: FastifyImp<{}, Device[], true> = async (
   request,
   reply,
 ) => {
+  const sqlForThisDevice = sql<boolean>`CASE WHEN ${devices.auth_token} = ${request.authentication_token} THEN TRUE ELSE FALSE END AS this_device`;
   const usersDevices = await db
-    .select(DEVICES_PUBLIC_OBJECT)
+    .select({
+      ...DEVICES_PUBLIC_OBJECT,
+      this_device: sqlForThisDevice,
+    })
     .from(devices)
     .where(eq(devices.user, request.user.id));
 
