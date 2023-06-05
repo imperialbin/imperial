fe6ebb8b110e1618d81e4047c5a8aaadc32f5f17
Cody Miller <hello@looskie.com>
2023-06-04 20:34:29 -0400

    api (feat): setRole utility for Discord

--- apps/api/src/utils/discord.ts
+++ apps/api/src/utils/discord.ts
@@ -1,5 +1,11 @@
 import { env } from "./env";
 import { DiscordUser } from "../types";
+import { Logger } from "@imperial/commons";
+
+const ROLES = {
+  "member-plus": env.DISCORD_ROLE_MEMBER_PLUS,
+  contributor: env.DISCORD_ROLE_CONTRIBUTOR,
+};
 
 export class Discord {
   public static async getAccessToken(code: string) {
@@ -53,5 +59,40 @@ export class Discord {
       .catch(() => null);
 
     return userRequest;
+  }
+
+  public static async setRole(userId: string, role: keyof typeof ROLES) {
+    const roleId = ROLES[role];
+
+    const roleRequest = await fetch(
+      `https://discord.com/api/guilds/${env.DISCORD_GUILD}/members/${userId}/roles/${roleId}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`,
+        },
+      },
+    )
+      .then(async (res) => {
+        if (!res.ok) {
+          const json = (await res.json()) as {
+            message: string;
+            code: number;
+          };
+          Logger.error(
+            "Discord",
+            `Error setting role ${json?.message} ${json?.code}`,
+          );
+          return false;
+        }
+
+        return true;
+      })
+      .catch((err) => {
+        Logger.error("Discord", `Error setting role ${String(err)}`);
+        return false;
+      });
+
+    return roleRequest;
   }
 }
